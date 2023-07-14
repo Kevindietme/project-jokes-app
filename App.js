614bcfc2bcc9dff0b6831810b418582d251034fd
Kevin Dietmeyer <kevindietmeyer@Kevins-MacBook-Air-2.local>
2023-07-14 11:46:42 -0400

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity } from 'react-native';
+import { styles } from './src/styles';
+import { useEffect, useState, Fragment } from 'react';
+import { shuffle } from './src/utils';
 
 export default function App() {
-  return (
+
+  const [jokes, setJokes] = useState();
+  const [i, setI] = useState(0);
+  const [reveal, setReveal] = useState(false);
+
+  const getJokes = async () => {
+    const resp = await fetch('https://api.sampleapis.com/jokes/goodJokes');
+    const json = await resp.json();
+    setJokes(shuffle(json));
+  }
+  useEffect(() => {getJokes(); }, []);
+
+  useEffect(() => {setReveal(false); }, [i]);
+
+  const getNextJoke = () => {
+    if(i < jokes.length - 1) setI(i + 1)
+    else setI(0)
+  }
+
+  return(
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <>
+    {!jokes
+      ? <Text style={styles.text}>Loading Jokes...</Text>
+      : <Fragment>
+        <Text style={styles.jokeText}>{jokes[i].setup}</Text>
+        <TouchableOpacity onPress={() => setReveal(!reveal)}
+        style={reveal ? styles.show : styles.hidden}>
+          {reveal
+            ? <Text style={styles.punchline}>{jokes[i].punchline}</Text>
+            : <Text style={styles.touchToReveal}>Touch for punchline</Text>
+          }
+        </TouchableOpacity>
+          <TouchableOpacity style={styles.bigButton} onPress={getNextJoke}>
+            <Text style={styles.buttonText}>Tell Me Another</Text>
+          </TouchableOpacity>
+      </Fragment>
+}   
+      </>
       <StatusBar style="auto" />
     </View>
-  );
+    );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
